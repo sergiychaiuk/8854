0879d8c48a30af938ec42f2a0dc83a39b8529b0c
serhiichaiuk <chayck7@gmail.com>
2022-11-19 21:00:57 +0200

    added an id property to the user state

--- src/store/index.js
+++ src/store/index.js
@@ -3,25 +3,33 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     user: {
+      id: 1,
       name: 'Eva Jonson',
       position: 'Sales Manager',
       offer: 'I will find the best offers for you. My services are absolutely free.',
       photo: 'assets/unsplash_O3ymvT7Wf9U.png',
       services: [
-        { title: 'Manual tour booking', number: 11 },
-        { title: 'Package tours', number: 3 },
-        { title: 'Hotels', number: 1 }
+        { id: 1, title: 'Manual tour booking', number: 11 },
+        { id: 2, title: 'Package tours', number: 3 },
+        { id: 3, title: 'Hotels', number: 1 }
       ],
       likes: 131,
       reviews: [
-        { name: 'Samuel Jackson', content: "Hey Eva! You're cool. Nice pic!", date: '13 Apr 2022' },
         {
+          id: 3,
+          name: 'Samuel Jackson',
+          content: "Hey Eva! You're cool. Nice pic!",
+          date: '13 Apr 2022'
+        },
+        {
+          id: 2,
           name: 'Angela Deimon',
           content:
             'Thanks for your services! We really liked the ocean view room. We hope to cooperate in the near future. We are sure you will do everything to make our next holiday fantastic.',
           date: '10 Apr 2022'
         },
         {
+          id: 1,
           name: 'Ronald Harris',
           content:
             'Eva, hello! There is such a question: How can I contact you if I am abroad in roaming?',
