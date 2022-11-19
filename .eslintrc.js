a0331a9247097321627ba514341bfcdf976bfdb7
serhiichaiuk <chayck7@gmail.com>
2022-11-19 15:05:03 +0200

    edited .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    'vue/setup-compiler-macros': true
   },
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-multi-spaces': ['error'],
+    'vue/no-multiple-template-root': 'off',
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-unused-vars': 'off',
+    'vue/script-setup-uses-vars': 'error'
   }
 }
